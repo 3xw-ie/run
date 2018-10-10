0c04dcbf1c259586548a76f503a4989cbd33555e
Dave Calnan <d@ve.ie>
2018-10-10 07:12:25 +0100

    Small fix: title

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -3,7 +3,7 @@
     <navbar class="min-h-screen w-1/5 bg-white flex flex-col p-4 pt-20 items-center"/>
     <section class="w-4/5 flex flex-col">
       <header class="bg-blue text-white p-4 border-b-2">
-        <!-- <h1>{{ dashboard.title ? dashboard.title : 'Dashboard' }}</h1> -->
+        <h1>{{ dashboard.title ? dashboard.title : 'Dashboard' }}</h1>
       </header>
       <nuxt class="flex-1 p-4 border-l-2 bg-grey-lightest"/>
     </section>
