6ffe379a3f59d8954e4bac82fe42cea786fa2a9c
Dave Calnan <d@ve.ie>
2018-10-11 07:38:01 +0100

    Tinkering

--- middleware/oauth.js
+++ middleware/oauth.js
@@ -11,15 +11,15 @@ export default function({ app, query, redirect }) {
     data: {
       code: query.code,
       client_id: '15329d68-6aaf-47ab-84ad-09d8d73f97be',
-      clianet_secret: '2b8eea09-0c43-4d9c-9b9b-7b2961cd41c2'
+      client_secret: '2b8eea09-0c43-4d9c-9b9b-7b2961cd41c2'
     }
   })
     .then(response => {
-      // axios({
-      //   url: 'http://postb.in/VGNb31jp',
-      //   method: 'post',
-      //   data: response
-      // })
+      axios({
+        url: 'http://postb.in/VGNb31jp',
+        method: 'post',
+        data: response
+      })
     })
     .catch(error => console.error(error))
 }
