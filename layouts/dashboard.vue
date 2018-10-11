647d871c796c75b0659078d7571c1fe73cb3b8de
Dave Calnan <d@ve.ie>
2018-10-11 05:12:08 +0100

    Update header color

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,11 +1,11 @@
 <template>
   <main class="min-h-screen w-full flex flex-col">
-    <header :style="'background-color:' + dashboard.color" class="flex justify-between bg-blue text-white p-4 border-b-2">
+    <header :style="'background-color:' + dashboard.primaryColor" class="flex justify-between bg-blue text-white p-4 border-b-2">
       <h1>{{ dashboard.title ? dashboard.title : 'Dashboard' }}</h1>
       <navbar class="flex items-center text-white"/>
     </header>
     <section class="flex-1 flex flex-col">
-      <nuxt :style="'background-color: ' + dashboard.backgroundColor" class="flex-1 p-4 border-l-2 bg-grey-lightest"/>
+      <nuxt :style="'background-color:' + dashboard.backgroundColor" class="flex-1 p-4 border-l-2 bg-grey-lightest"/>
     </section>
   </main>
 </template>
