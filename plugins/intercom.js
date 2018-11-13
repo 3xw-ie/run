7ff9cdd01f00d4f2dcbc9861a5d37374ac7cc16e
Dave Calnan <d@ve.ie>
2018-11-13 22:25:17 +0000

    Update run-api baseURL

--- plugins/intercom.js
+++ plugins/intercom.js
@@ -14,7 +14,9 @@ const intercom = {
   computed: {
     intercomConfig() {
       return {
-        baseURL: `https://api.3xw.app/intercom/${this.account.intercomToken}`
+        baseURL: `https://api.3xw.app/rest/intercom/${
+          this.account.intercomToken
+        }`
       }
     },
     ...mapGetters(['account'])
